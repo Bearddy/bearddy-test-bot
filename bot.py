018d798b431f250d44c4309826fa7f47fe15bf90
Bearddy <choiyeiin9235@gmail.com>
2021-09-04 22:22:31 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,31 +23,34 @@ async def on_message(message):
         await message.channel.send("곰띠봇 ===== //////// ------ 테스트 메시지 입니다")
 
     if message.content == "곰띠님 도와줘":
-        await message.channel.send(embed=cmd_list())
+        cmd_list(message)
 
     if message.content == "곰띠님 알려줘 리스트":
-        await message.channel.send(embed=tell_list())
+        tell_list(message)
 
     if message.content == "곰띠님 놀아줘 리스트":
-        await message.channel.send(embed=play_list())
+        play_list(message)
 
     if message.content == "곰띠님":
-        await message.channel.send(hi_bearddy())
+        hi_bearddy(message)
 
     if message.content.startswith("곰띠님 알려줘 execute"):
-        await message.channel.send(embed=cmd_execute_help())
+        cmd_execute_help(message)
 
     if message.content.startswith("곰띠님 알려줘 tp"):
-        await message.channel.send(embed=cmd_tp_help())
+        cmd_tp_help(message)
 
     if message.content.startswith("곰띠님 알려줘 setblock"):
-        await message.channel.send(embed=cmd_setblock_help())
+        cmd_setblock_help(message)
     
     if message.content.startswith("곰띠님 놀아줘 랜덤숫자 "):
-        await message.channel.send(random_num(message))
+        random_num(message)
 
     if message.content.startswith("곰띠님 놀아줘 랜덤단어 "):
-        await message.channel.send(random_word(message))
+        random_word(message)
+    
+    if message.content.startswith("곰띠님 투표해줘 "):
+        vote(message)
 
 
 
